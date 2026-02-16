4c38ed8a0fbf0e46f12dfdab70c17350f77ffeab
shaury-singh <rathore.singh.shaury@gmail.com>
2026-02-16 14:47:56 +0530

    Bugs Fixed

--- engine.js
+++ engine.js
@@ -1,7 +1,10 @@
+import {addOrderIntoDatabase, addMatchedOrderIntoDatabase} from "./database.js";
+
 class Engine {
     constructor() {
-        this.sellBook = {};
-        this.buyBook = {};
+        this.sellBook = [];
+        this.buyBook = [];
+        this.dbQueue = [];
     }
 
     initializeShare(shareIndex) {
@@ -9,7 +12,7 @@ class Engine {
         if (!this.buyBook[shareIndex]) this.buyBook[shareIndex] = [];
     }
 
-    async enqueueSellOrder(shareName, value, qty, time, currIdx, shareIndex) {
+    async enqueueSellOrder(shareName, value, qty, time, currIdx, orderID, shareIndex) {
         this.initializeShare(shareIndex);
         if (currIdx === this.sellBook[shareIndex].length - 1) {
             this.sellBook[shareIndex].push({ shareName, value, time, qty });
@@ -22,13 +25,13 @@ class Engine {
                 let temp = this.sellBook[shareIndex][currIdx];
                 this.sellBook[shareIndex][currIdx] = this.sellBook[shareIndex][parentIdx];
                 this.sellBook[shareIndex][parentIdx] = temp;
-                return this.enqueueSellOrder(shareName, value, qty, time, parentIdx, shareIndex);
+                return this.enqueueSellOrder(shareName, value, qty, time, parentIdx, orderID, shareIndex);
             } else if (this.sellBook[shareIndex][currIdx].value === this.sellBook[shareIndex][parentIdx].value) {
                 if (this.sellBook[shareIndex][currIdx].time < this.sellBook[shareIndex][parentIdx].time) {
                     let temp = this.sellBook[shareIndex][currIdx];
                     this.sellBook[shareIndex][currIdx] = this.sellBook[shareIndex][parentIdx];
                     this.sellBook[shareIndex][parentIdx] = temp;
-                    return this.enqueueSellOrder(shareName, value, qty, time, parentIdx, shareIndex);
+                    return this.enqueueSellOrder(shareName, value, qty, time, parentIdx, orderID, shareIndex);
                 } else if (
                     this.sellBook[shareIndex][currIdx].time === this.sellBook[shareIndex][parentIdx].time &&
                     this.sellBook[shareIndex][currIdx].qty > this.sellBook[shareIndex][parentIdx].qty
@@ -36,13 +39,13 @@ class Engine {
                     let temp = this.sellBook[shareIndex][currIdx];
                     this.sellBook[shareIndex][currIdx] = this.sellBook[shareIndex][parentIdx];
                     this.sellBook[shareIndex][parentIdx] = temp;
-                    return this.enqueueSellOrder(shareName, value, qty, time, parentIdx, shareIndex);
+                    return this.enqueueSellOrder(shareName, value, qty, time, parentIdx, orderID, shareIndex);
                 }
             }
         }
     }
 
-    async enqueueBuyOrder(shareName, value, qty, time, currIdx, shareIndex) {
+    async enqueueBuyOrder(shareName, value, qty, time, currIdx, orderID, shareIndex) {
         this.initializeShare(shareIndex);
         if (currIdx === this.buyBook[shareIndex].length - 1) {
             this.buyBook[shareIndex].push({ shareName, value, time, qty });
@@ -55,13 +58,13 @@ class Engine {
                 let temp = this.buyBook[shareIndex][currIdx];
                 this.buyBook[shareIndex][currIdx] = this.buyBook[shareIndex][parentIdx];
                 this.buyBook[shareIndex][parentIdx] = temp;
-                return this.enqueueBuyOrder(shareName, value, qty, time, parentIdx, shareIndex);
+                return this.enqueueBuyOrder(shareName, value, qty, time, parentIdx, orderID, shareIndex);
             } else if (this.buyBook[shareIndex][currIdx].value === this.buyBook[shareIndex][parentIdx].value) {
                 if (this.buyBook[shareIndex][currIdx].time < this.buyBook[shareIndex][parentIdx].time) {
                     let temp = this.buyBook[shareIndex][currIdx];
                     this.buyBook[shareIndex][currIdx] = this.buyBook[shareIndex][parentIdx];
                     this.buyBook[shareIndex][parentIdx] = temp;
-                    return this.enqueueBuyOrder(shareName, value, qty, time, parentIdx, shareIndex);
+                    return this.enqueueBuyOrder(shareName, value, qty, time, parentIdx, orderID, shareIndex);
                 } else if (
                     this.buyBook[shareIndex][currIdx].time === this.buyBook[shareIndex][parentIdx].time &&
                     this.buyBook[shareIndex][currIdx].qty > this.buyBook[shareIndex][parentIdx].qty
@@ -69,7 +72,7 @@ class Engine {
                     let temp = this.buyBook[shareIndex][currIdx];
                     this.buyBook[shareIndex][currIdx] = this.buyBook[shareIndex][parentIdx];
                     this.buyBook[shareIndex][parentIdx] = temp;
-                    return this.enqueueBuyOrder(shareName, value, qty, time, parentIdx, shareIndex);
+                    return this.enqueueBuyOrder(shareName, value, qty, time, parentIdx, orderID, shareIndex);
                 }
             }
         }
@@ -184,24 +187,62 @@ class Engine {
         }
         return orderObj;
     }
+
+    fetchCurrentMarketValue(upperCircuit, lowerCircuit,shareIndex){
+        if (this.sellBook[shareIndex].length == 0){
+            if (this.buyBook[shareIndex].length == 0){
+                return {"value":lowerCircuit,"statusCode":1};
+            }
+            return {"value":upperCircuit,"statusCode":2};
+        } else if (this.buyBook[shareIndex].length == 0 ){
+            return {"value":lowerCircuit,"statusCode":4};
+        } else if (this.sellBook[shareIndex][0].value <= this.buyBook[shareIndex][0].value){
+            return {"value":this.sellBook[shareIndex][0].value,"statusCode":3};
+        } else {
+            return {"value":(this.sellBook[shareIndex][0].value + this.buyBook[shareIndex][0].value)/2,"statusCode":5};
+        }
+    }
+
+    async matchOrders(shareIndex){
+        while (this.sellBook[shareIndex].length != 0 && this.buyBook[shareIndex].length != 0 && this.buyBook[shareIndex][0].value >= this.sellBook[shareIndex][0].value){
+            let qty = this.sellBook[shareIndex][0].qty - this.buyBook[shareIndex][0].qty;
+            if (qty > 0){
+                await addMatchedOrderIntoDatabase(this.sellBook[shareIndex][0].value,Math.min(this.sellBook[shareIndex][0].qty,this.buyBook[shareIndex][0].qty),"JSW","Shaury Singh","Vedant Ere");
+                let buyOrder = this.dequeuefromBuyBook(shareIndex); 
+                this.sellBook[shareIndex][0].qty = qty;
+                console.log(`${JSON.stringify(this.sellBook[shareIndex][0])} matched to ${JSON.stringify(buyOrder)}`);
+            } else if (qty == 0) {
+                await addMatchedOrderIntoDatabase(this.sellBook[shareIndex][0].value,this.sellBook[shareIndex][0].qty,"JSW","Shaury Singh","Vedant Ere");
+                let sellOrder = this.dequeuefromSellBook(shareIndex);
+                let buyOrder = this.dequeuefromBuyBook(shareIndex); 
+                console.log(`${JSON.stringify(sellOrder)} matched to ${JSON.stringify(buyOrder)}`);
+            } else {
+                await addMatchedOrderIntoDatabase(this.sellBook[shareIndex][0].value,Math.min(this.sellBook[shareIndex][0].qty,this.buyBook[shareIndex][0].qty),"JSW","Shaury Singh","Vedant Ere");
+                let sellOrder = this.dequeuefromSellBook(shareIndex);
+                this.buyBook[shareIndex][0].qty = (qty*-1);
+                console.log(`${JSON.stringify(sellOrder)} matched to ${JSON.stringify(this.buyBook[shareIndex][0])}`);
+            }
+        }
+        console.log(`Current Market Value is: ${JSON.stringify(this.fetchCurrentMarketValue(150.23,200.48,shareIndex))}`);
+    }
 }
 
 async function runEngine() {
     const engine = new Engine();
     const shareIndex = 0;
     engine.initializeShare(shareIndex);
-    await engine.enqueueSellOrder("JSW", 148.55, 5, Date.now(), engine.sellBook[shareIndex].length - 1, shareIndex);
-    await engine.enqueueSellOrder("JSW", 148.70, 3, Date.now(), engine.sellBook[shareIndex].length - 1, shareIndex);
-    await engine.enqueueSellOrder("JSW", 148.35, 10, Date.now(), engine.sellBook[shareIndex].length - 1, shareIndex);
-    await engine.enqueueSellOrder("JSW", 148.15, 13, Date.now(), engine.sellBook[shareIndex].length - 1, shareIndex);
-    await engine.enqueueSellOrder("JSW", 148.85, 28, Date.now(), engine.sellBook[shareIndex].length - 1, shareIndex);
-    await engine.enqueueBuyOrder("JSW", 148.35, 1, Date.now(), engine.buyBook[shareIndex].length - 1, shareIndex);
-    await engine.enqueueBuyOrder("JSW", 148.40, 3, Date.now(), engine.buyBook[shareIndex].length - 1, shareIndex);
-    await engine.enqueueBuyOrder("JSW", 148.38, 50, Date.now(), engine.buyBook[shareIndex].length - 1, shareIndex);
-    await engine.enqueueSellOrder("JSW", 148.15, 2, Date.now(), engine.sellBook[shareIndex].length - 1, shareIndex);
+    await engine.enqueueSellOrder("JSW", 148.55, 5, Date.now(), engine.sellBook[shareIndex].length - 1, 1, shareIndex);
+    await engine.enqueueSellOrder("JSW", 148.70, 3, Date.now(), engine.sellBook[shareIndex].length - 1, 12, shareIndex);
+    await engine.enqueueSellOrder("JSW", 148.35, 10, Date.now(), engine.sellBook[shareIndex].length - 1, 13, shareIndex);
+    await engine.enqueueSellOrder("JSW", 148.15, 13, Date.now(), engine.sellBook[shareIndex].length - 1, 14, shareIndex);
+    await engine.enqueueSellOrder("JSW", 148.85, 28, Date.now(), engine.sellBook[shareIndex].length - 1, 15, shareIndex);
+    await engine.enqueueBuyOrder("JSW", 148.35, 1, Date.now(), engine.buyBook[shareIndex].length - 1, 16, shareIndex);
+    await engine.enqueueBuyOrder("JSW", 148.40, 3, Date.now(), engine.buyBook[shareIndex].length - 1, 17, shareIndex);
+    await engine.enqueueBuyOrder("JSW", 148.38, 50, Date.now(), engine.buyBook[shareIndex].length - 1, 18, shareIndex);
+    await engine.enqueueSellOrder("JSW", 148.15, 2, Date.now(), engine.sellBook[shareIndex].length - 1, 19, shareIndex);
     const sameTime = Date.now();
-    await engine.enqueueBuyOrder("JSW", 148.40, 10, sameTime, engine.buyBook[shareIndex].length - 1, shareIndex);
-    await engine.enqueueBuyOrder("JSW", 148.40, 2, sameTime, engine.buyBook[shareIndex].length - 1, shareIndex);
+    await engine.enqueueBuyOrder("JSW", 148.40, 10, sameTime, engine.buyBook[shareIndex].length - 1, 20, shareIndex);
+    await engine.enqueueBuyOrder("JSW", 148.40, 2, sameTime, engine.buyBook[shareIndex].length - 1, 21, shareIndex);
     await engine.matchOrders(shareIndex);
     console.log("------------- SELL BOOK ---------------");
     console.log(engine.sellBook[shareIndex]);
